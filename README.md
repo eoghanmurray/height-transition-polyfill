db98371e78932a567c7204715523c648d7c6ed7f
Eoghan Murray <eoghan@getthere.ie>
2019-11-15 13:41:19 +0000

    Initial base version; for prehistory see https://jsfiddle.net/EoghanM/Ls05w9qc/92/

--- README.md
+++ README.md
@@ -1,2 +1,50 @@
 # height-transition-polyfill
 A polyfill to get exact height transitions from zero to auto, when already using the max-height workaround
+
+This gets rid of delays due to mismatch between max-height and actual height.
+
+See this answer for more information: https://stackoverflow.com/a/8331169/6691
+
+E.g. the following:
+
+    <div id="menu">
+        <a>hover me</a>
+        <ul id="list">
+            <!-- Create a bunch, or not a bunch, of li's to see the timing. -->
+            <li>item</li>
+            <li>item</li>
+            <li>item</li>
+            <li>item</li>
+            <li>item</li>
+        </ul>
+    </div>
+
+    #menu #list {
+        max-height: 0;
+        transition: max-height 0.15s ease-out;
+        overflow: hidden;
+        background: #d5d5d5;
+    }
+
+    #menu:hover #list {
+        max-height: 500px;
+        transition: max-height 0.25s ease-in;
+    }
+
+Can have `max-height: 10000000px` to accommodate unknown content, and the transition will be converted on the fly to a `height` transition, starting from the current height of the DOM element.
+
+The script relies on an injected CSS rule `[height-locked] { height: 0 !important;}`.  For transitions down to non-zero heights, this rule will have to be manually overridden, e.g.
+
+    div.when-small {
+        max-height: 2em;
+        transition: max-height 0.15s ease-out;
+    }
+
+    div:not(.when-small) {
+        max-height: 30em;
+        transition: max-height 0.25s ease-in;
+    }
+
+    div.when-small[height-locked] {
+        height: 2em !important;
+    }
---
+++ height-transition-polyfill.js
@@ -0,0 +1,61 @@
+// start max-height -> height polyfill
+(function() {
+  var style = document.createElement("style");
+  style.appendChild(document.createTextNode("[height-locked] { height: 0 !important;}"));
+  document.head.appendChild(style);
+
+  function test_direction(el, starting_maxh) {
+    var self = function() {
+      var cs = getComputedStyle(el);
+      if (cs['maxHeight'] == starting_maxh) {
+        requestAnimationFrame(self);
+      } else {
+        var tr = cs['transition'];
+        if (cs['maxHeight'] < starting_maxh) {
+          // shrinking
+          if (el.style.height == '') {
+            el.style.height = cs['height'];
+            void el.offsetWidth; // needed for transition to 'take'
+          }
+          el.setAttribute('height-locked', true);
+        } else {
+          // growing
+          el.setAttribute('height-locked', true);
+          el.style.height = el.scrollHeight + 'px';
+          void el.offsetWidth; // transition happens now between zero (height-locked) to style.height
+          el.removeAttribute('height-locked');
+        }
+      }
+    }
+    // need requestAnimationFrame to detect the direction of the max-height transition
+    requestAnimationFrame(self);
+  }
+
+  document.addEventListener('transitionstart', function(e) {
+    if (e.propertyName == 'max-height') {
+      var el = e.target;
+      if (el.hasAttribute('height-locked')) {
+        el.removeAttribute('height-locked');
+      } else {
+        var cs = getComputedStyle(el);
+        var tr = cs['transition'];
+        var add_h_transition = tr.match(/max-(height[^,]+)/)[1];
+        if (el.style.transition.indexOf(add_h_transition) === -1) {
+          el.style.transition = tr + ', ' + add_h_transition;
+        }
+        test_direction(el, cs['maxHeight']);
+      }
+    }
+  });
+
+  document.addEventListener('transitionend', function(e) {
+    if (e.propertyName == 'height') {
+      var el = e.target;
+      var cs = getComputedStyle(el);
+      if (cs['height'] == el.style.height) {
+        el.style.height = '';
+        el.style.transition = '';
+      }
+    }
+  });
+}());
